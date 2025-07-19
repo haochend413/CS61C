50b13290744f4fa782322a4286c32024ddc0edd5
haochend413 <dinghaochen413@gmail.com>
2025-07-19 15:50:34 -0500

    finish lab02

--- CS61C-Assignment/Lab/lab02/lfsr.c
+++ CS61C-Assignment/Lab/lab02/lfsr.c
@@ -5,6 +5,16 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    int c = *reg;
+    int z = c & 1;
+    int t = (c >> 2) & 1;
+    int th = (c >> 3) & 1;
+    int f = (c >> 5) & 1;
+    int left = z ^ t ^ th ^ f; 
+    //16 bits; 
+    *reg = *reg >> 1;
+    if (left) {
+    *reg = (*reg) | (1 << 15);
+  }
 }
 
